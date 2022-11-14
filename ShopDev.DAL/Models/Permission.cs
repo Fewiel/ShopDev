5c02d171ba159e29de1030e950c527f03f487762
p.weitkamp <p.weitkamp@shopware.com>
2022-11-14 16:19:50 +0100

    add permission model

---
+++ ShopDev.DAL/Models/Permission.cs
@@ -0,0 +1,10 @@
+﻿using ShopDev.DAL.Interfaces;
+
+namespace ShopDev.DAL.Models;
+
+public class Permission : IDBIdentifier
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string InternalName { get; set; }
+}
