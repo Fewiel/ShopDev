fc91c5a83770dfc9834bd2c310d19d2dfd050d89
p.weitkamp <p.weitkamp@shopware.com>
2023-01-02 23:54:11 +0100

    start with node host

---
+++ ShopDev/Server/Services/NodeHost.cs
@@ -0,0 +1,48 @@
+﻿using ShopDev.DAL.Repositories;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ShopDev.Server.Services;
+
+public class NodeHost : IHostedService
+{
+    private bool Running;
+    private int Port;
+    private SettingRepository _setting;
+
+    public NodeHost(SettingRepository setting)
+    {
+        _setting = setting;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        Running = true;
+
+        var port = await _setting.GetByKeyAsync("node_server_port");
+
+        if (port == null || port.Value == null)
+            return;
+
+        Port = int.Parse(port.Value);
+
+
+        new Thread(Worker).Start();        
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        Running = false;
+        return Task.CompletedTask;
+    }
+
+    private void Worker()
+    {
+        var server = new TcpListener(IPAddress.Any, );
+
+        while (Running)
+        {
+            Thread.Sleep(100);
+        }
+    }
+}
